950d6c76558bfaeb6134854b1042975a51720ab9
JeetVaidya1 <113214646+JeetVaidya1@users.noreply.github.com>
2025-03-23 23:45:56 -0700

    Update config.php
    configured database for deployment

--- config/config.php
+++ config/config.php
@@ -1,7 +1,19 @@
 <?php
-define('DBHOST', '127.0.0.1');
-define('DBNAME', 'bookhive_db');
-define('DBUSER', 'root');
-define('DBPASS', '');
+$onServer = strpos($_SERVER['HTTP_HOST'], 'cosc360.ok.ubc.ca') !== false;
+
+if ($onServer) {
+    // Server settings
+    define('DBHOST', 'localhost');
+    define('DBNAME', 'yourcwl');
+    define('DBUSER', 'yourcwl');
+    define('DBPASS', 'yourcwl');
+} else {
+    // Local dev settings
+    define('DBHOST', '127.0.0.1');
+    define('DBNAME', 'bookhive_db');
+    define('DBUSER', 'root');
+    define('DBPASS', '');
+}
+
 define('DBCONNSTRING',"mysql:host=". DBHOST. ";dbname=". DBNAME);
 ?>
